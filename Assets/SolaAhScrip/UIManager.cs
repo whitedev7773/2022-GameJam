a3ae42789d305a0ab42e04c399dd98778ffc8b04
CAFUCH <98962616+CAFUCH@users.noreply.github.com>
2022-08-20 16:15:49 +0900

    에셋 추가, 주문 받는 스크립트 작성 시작

--- Assets/SolaAhScrip/UIManager.cs
+++ Assets/SolaAhScrip/UIManager.cs
@@ -5,7 +5,7 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    private ironbutton ironButton = null;
+    private ironbutton naB = null;
     //각 스크립트에서 고철, 고무, 기름, 코인의 값을 받아와서 표시
     [SerializeField] private TMP_Text coinTxt = null;
     private int coins = 0;
@@ -15,17 +15,16 @@ public class UIManager : MonoBehaviour
     private int rubbers = 0;
     [SerializeField] private TMP_Text oilTxt = null;
     private int oils = 0;
-
     private void Awake() {
-        ironButton = GameObject.Find("").GetComponent<ironbutton>();
+        naB = GetComponent<ironbutton>();
     }
     private void Update() {
         Txts();
     }
     private void Txts() {
         // coinTxt.text = + "개";
-        ironTxt.text = ironButton.iron + "개";
+        // ironTxt.text = naB.iron + "개";
         // rubberTxt.text = ironButton.rubber + "개";
-        oilTxt.text = ironButton.iron + "개";
+        oilTxt.text = naB.iron + "개";
     }
 }
