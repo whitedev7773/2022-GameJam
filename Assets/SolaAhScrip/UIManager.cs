67eec3ea9584dba98250aab959ad96a5a451a90a
CAFUCH <98962616+CAFUCH@users.noreply.github.com>
2022-08-20 11:50:54 +0900

    인트로씬 제작, 게임 배경 화면 Sprite, UI 값 가져오려다 나영이가 괴롭혀서 실패

--- Assets/SolaAhScrip/UIManager.cs
+++ Assets/SolaAhScrip/UIManager.cs
@@ -5,17 +5,27 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+    private ironbutton ironButton = null;
     //각 스크립트에서 고철, 고무, 기름, 코인의 값을 받아와서 표시
-    [SerializeField] private TMP_Text coinTxt;
+    [SerializeField] private TMP_Text coinTxt = null;
     private int coins = 0;
-    [SerializeField] private TMP_Text tlqkfTxt;
-    private int tlqkf = 0;
-    [SerializeField] private TMP_Text oilTxt;
+    [SerializeField] private TMP_Text ironTxt = null;
+    private int irons = 0;
+    [SerializeField] private TMP_Text rubberTxt = null;
+    private int rubbers = 0;
+    [SerializeField] private TMP_Text oilTxt = null;
     private int oils = 0;
-    [SerializeField] private TMP_Text dkdkdkd;
-    private int dkdkdkdk = 0;
 
     private void Awake() {
-        
+        ironButton = GameObject.Find("").GetComponent<ironbutton>();
+    }
+    private void Update() {
+        Txts();
+    }
+    private void Txts() {
+        // coinTxt.text = + "개";
+        ironTxt.text = ironButton.iron + "개";
+        // rubberTxt.text = ironButton.rubber + "개";
+        oilTxt.text = ironButton.iron + "개";
     }
 }
