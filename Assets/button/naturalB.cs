ac9c155aabad2cf68df773ad99f90b143bcf267f
nayoung01 <rlaskdudglgl@naver.com>
2022-08-20 14:28:59 +0900

    커밋했다~

--- Assets/button/naturalB.cs
+++ Assets/button/naturalB.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class ironButton : MonoBehaviour
+public class naturalB : MonoBehaviour
 {
     public Button ironB, rubberB, oilB;
 
